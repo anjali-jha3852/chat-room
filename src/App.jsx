1aedc4282654fed5a2ef6b5f8ad3cdcf6eaa5165
anjali-jha <anjalicu3852@gmail.com>
2025-09-25 19:59:30 +0530

    Update frontend for Vercel deployment, fix API URL, and update configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { io } from "socket.io-client";
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
+  server: {
+    host: '0.0.0.0',   // bind to all network interfaces
+    port: 5173,         // fixed port
+    strictPort: true,   // fail if port is in use
+  }
 })
